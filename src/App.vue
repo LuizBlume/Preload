7f124f8feb7bb6f077120d14a9ff15b8be87013d
LuizBlume <luiz.fernando.251107@gmail.com>
2024-07-28 01:41:55 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Homeview from './views/HomeView.vue'
+</script>
+
+<template>
+  <Homeview />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/PreloadComp.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+const loading = ref(true);
+</script>
+<template>
+    <div class="container">
+        <div class="box-load" v-if="loading">
+            <div class="preload"></div>
+        </div>
+    </div>
+</template>
+<style scoped>
+@import '../assets/style.css';
+</style>
